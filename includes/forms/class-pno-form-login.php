29d719ff4053e7bc2e04a6b88aa15108ee9505b1
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-07-08 12:01:39 +0100

    add comments for filters

--- includes/forms/class-pno-form-login.php
+++ includes/forms/class-pno-form-login.php
@@ -46,45 +46,73 @@ class PNO_Form_Login extends PNO_Form {
 	public function __construct() {
 		add_action( 'wp', array( $this, 'process' ) );
 
-		$this->steps = (array) apply_filters(
-			'pno_login_form_steps',
-			array(
-				'submit'  => array(
-					'name'     => __( 'Login Details' ),
-					'view'     => array( $this, 'submit' ),
-					'handler'  => array( $this, 'submit_handler' ),
-					'priority' => 10,
-				),
-				'login' => array(
-					'name'     => false,
-					'view'     => false,
-					'handler'  => array( $this, 'login_handler' ),
-					'priority' => 20,
-				)
+		$steps = array(
+			'submit'  => array(
+				'name'     => __( 'Login Details' ),
+				'view'     => array( $this, 'submit' ),
+				'handler'  => array( $this, 'submit_handler' ),
+				'priority' => 10,
+			),
+			'login' => array(
+				'name'     => false,
+				'view'     => false,
+				'handler'  => array( $this, 'login_handler' ),
+				'priority' => 20,
 			)
 		);
+
+		/**
+		 * List of steps for the login form.
+		 *
+		 * By default there are 2 steps: the first is used to validate the user's account,
+		 * the second is used to actually log the user in.
+		 *
+		 * Each step has a "view" method that handles what's displayed on the frontend and an "handler"
+		 * method that processes what's submitted through the fields within the "view" step.
+		 *
+		 * @since 0.1.0
+		 * @param array $steps the list of steps for the login form.
+		 */
+		$this->steps = (array) apply_filters( 'pno_login_form_steps', $steps );
 
 		uasort( $this->steps, array( $this, 'sort_by_priority' ) );
 
 	}
 
+	/**
+	 * Defines the fields of the login form.
+	 *
+	 * @return void
+	 */
 	public function init_fields() {
-		$this->fields = apply_filters(
-			'pno_login_form_fields',
-			array(
-				'login' => array(
-					'test'       => array(
-						'label'       => __( 'Test' ),
-						'type'        => 'text',
-						'required'    => true,
-						'placeholder' => '',
-						'priority'    => 1,
-					),
+
+		$fields = array(
+			'login' => array(
+				'test' => array(
+					'label'       => __( 'Test' ),
+					'type'        => 'text',
+					'required'    => true,
+					'placeholder' => '',
+					'priority'    => 1,
 				),
-			)
+			),
 		);
+
+		/**
+		 * Allows developers to register or deregister fields for the login form.
+		 *
+		 * @since 0.1.0
+		 * @param array $fields array containing the list of fields for the login form.
+		 */
+		$this->fields = apply_filters( 'pno_login_form_fields', $fields );
+
 	}
 
+	/**
+	 * Handles the display of the login form.
+	 *
+	 * @return void
+	 */
 	public function submit() {
 
 		$this->init_fields();
