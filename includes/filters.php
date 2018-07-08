05b39d4841940181c98519c596338857e5a21156
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-07-08 19:57:14 +0100

    modify the authentication methods

---
+++ includes/filters.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * List of filters that should only trigger on the frontend.
+ *
+ * @package     posterno
+ * @copyright   Copyright (c) 2018, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Validate authentication with the selected login method.
+ *
+ * @param object $user
+ * @param string $username
+ * @param string $password
+ * @return void
+ */
+function pno_authentication( $user, $username, $password ) {
+
+	$authentication_method = pno_get_option( 'login_method' );
+
+	if ( $authentication_method == 'username' ) {
+
+		if ( is_email( $username ) ) {
+			return new WP_Error( 'username_only', __( 'Invalid username or incorrect password.' ) );
+		}
+		return wp_authenticate_username_password( null, $username, $password );
+
+	} elseif ( $authentication_method == 'email' ) {
+
+		if ( ! empty( $username ) && is_email( $username ) ) {
+
+			$user = get_user_by( 'email', $username );
+
+			if ( isset( $user, $user->user_login, $user->user_status ) && 0 == (int) $user->user_status ) {
+				$username = $user->user_login;
+				return wp_authenticate_username_password( null, $username, $password );
+			}
+		} else {
+			return new WP_Error( 'email_only', __( 'Invalid email address or incorrect password.' ) );
+		}
+	}
+
+	return $user;
+
+}
+add_filter( 'authenticate', 'pno_authentication', 20, 3 );
