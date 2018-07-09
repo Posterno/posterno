7fa8519916cbac1094328f8c299d98a7f3583440
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-07-09 16:07:13 +0100

    added functions to retrive all required pages ids

--- includes/functions.php
+++ includes/functions.php
@@ -28,3 +28,75 @@ function pno_get_login_page_id() {
 	return $login_page;
 
 }
+
+/**
+ * Retrieve the ID number of the selected registration page.
+ *
+ * @return mixed
+ */
+function pno_get_registration_page_id() {
+
+	$registration_page = false;
+	$page_option       = pno_get_option( 'registration_page' );
+
+	if ( is_array( $page_option ) && isset( $page_option['value'] ) ) {
+		$registration_page = absint( $page_option['value'] );
+	}
+
+	return $registration_page;
+
+}
+
+/**
+ * Retrieve the ID number of the selected password recovery page.
+ *
+ * @return mixed
+ */
+function pno_get_password_recovery_page_id() {
+
+	$password_page = false;
+	$page_option   = pno_get_option( 'password_page' );
+
+	if ( is_array( $page_option ) && isset( $page_option['value'] ) ) {
+		$password_page = absint( $page_option['value'] );
+	}
+
+	return $password_page;
+
+}
+
+/**
+ * Retrieve the ID number of the selected password recovery page.
+ *
+ * @return mixed
+ */
+function pno_get_dashboard_page_id() {
+
+	$dashboard_page = false;
+	$page_option    = pno_get_option( 'dashboard_page' );
+
+	if ( is_array( $page_option ) && isset( $page_option['value'] ) ) {
+		$dashboard_page = absint( $page_option['value'] );
+	}
+
+	return $dashboard_page;
+
+}
+
+/**
+ * Retrieve the ID number of the selected password recovery page.
+ *
+ * @return mixed
+ */
+function pno_get_profile_page_id() {
+
+	$profile_page = false;
+	$page_option  = pno_get_option( 'profile_page' );
+
+	if ( is_array( $page_option ) && isset( $page_option['value'] ) ) {
+		$profile_page = absint( $page_option['value'] );
+	}
+
+	return $profile_page;
+
+}
