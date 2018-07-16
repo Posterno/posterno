f6de62ad992a763373166c0c93c291793b17a191
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-07-16 18:18:26 +0100

    load custom fields page within the backend

--- includes/admin/admin-pages.php
+++ includes/admin/admin-pages.php
@@ -20,7 +20,7 @@ function pno_add_admin_pages() {
 
 	global $pno_settings_page, $pno_custom_fields_page;
 
-	$pno_custom_fields_page = add_submenu_page( 'edit.php?post_type=listings', __( 'Custom fields' ), __( 'Custom fields' ), 'manage_options', 'posterno-custom-fields', 'pno_custom_fields' );
+	$pno_custom_fields_page = add_submenu_page( 'edit.php?post_type=listings', __( 'Custom fields' ), __( 'Custom fields' ), 'manage_options', 'posterno-custom-fields', 'pno_custom_fields_page' );
 	$pno_settings_page      = add_submenu_page( 'edit.php?post_type=listings', __( 'Posterno Settings' ), __( 'Settings' ), 'manage_options', 'posterno-settings', 'pno_options_page' );
 }
 add_action( 'admin_menu', 'pno_add_admin_pages', 10 );
---
+++ includes/admin/custom-fields/display-custom-fields.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Displays the content of the custom fields editor page.
+ *
+ * @package     posterno
+ * @copyright   Copyright (c) 2018, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Function responsible of displaying the custom fields page.
+ * Actual output handled by vuejs.
+ *
+ * @since 0.1.0
+ * @return void
+ */
+function pno_custom_fields_page() {
+	echo '<div id="posterno-custom-fields-page"></div>';
+}
