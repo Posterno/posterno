635fa6663114a67b91f11b497cdc3f4ec935bc2f
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-07-16 18:16:21 +0100

    registers a custom fields page for the admin panel

--- includes/admin/admin-pages.php
+++ includes/admin/admin-pages.php
@@ -18,9 +18,9 @@ defined( 'ABSPATH' ) || exit;
  */
 function pno_add_admin_pages() {
 
-	global $pno_settings_page;
+	global $pno_settings_page, $pno_custom_fields_page;
 
-	$pno_settings_page = add_submenu_page( 'edit.php?post_type=listings', __( 'Posterno Settings' ), __( 'Settings' ), 'manage_options', 'posterno-settings', 'pno_options_page' );
-
+	$pno_custom_fields_page = add_submenu_page( 'edit.php?post_type=listings', __( 'Custom fields' ), __( 'Custom fields' ), 'manage_options', 'posterno-custom-fields', 'pno_custom_fields' );
+	$pno_settings_page      = add_submenu_page( 'edit.php?post_type=listings', __( 'Posterno Settings' ), __( 'Settings' ), 'manage_options', 'posterno-settings', 'pno_options_page' );
 }
 add_action( 'admin_menu', 'pno_add_admin_pages', 10 );
