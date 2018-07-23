c01fadd81247afd3419bc6405f3da45e77bbb2d0
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-07-23 13:29:23 +0100

    Make the required setting for the email field always required.

--- assets/js/src/pno-profile-custom-fields-admin-validation.js
+++ assets/js/src/pno-profile-custom-fields-admin-validation.js
@@ -1,18 +1,9 @@
 $(document).on('carbonFields.apiLoaded', function (e, api) {
 	$(document).on('carbonFields.validateField', function (e, fieldName, error) {
-		console.log('Field being validated: ' + fieldName);
-		console.log('Current error, if any: ' + error);
 
-		// Add your own validation logic here
-		// To raise an error return any string which will serve as the user-friendly error message
-		// To not raise an error return `null`
-		// To not interfere with the built-in validation return the `error` variable argument
-
-		// This example will raise an error if the field's value is not an even number
-		// If the value is even, it will proceed with validation as usual
-		if (fieldName === 'field_meta_key' && pno_user_cf.is_default ) {
+		if (fieldName === 'field_meta_key' && pno_user_cf.is_default) {
 			var value = api.getFieldValue(fieldName);
-			if ( value !== pno_user_cf.field_id ) {
+			if (value !== pno_user_cf.field_id) {
 				return pno_user_cf.messages.no_meta_key_changes;
 			}
 		}
@@ -26,4 +17,14 @@ $(document).on('carbonFields.apiLoaded', function (e, api) {
 
 		return error;
 	});
+
+	// Make the required setting for the email field always required.
+	$(document).on('carbonFields.fieldUpdated', function (e, fieldName) {
+		if (fieldName === 'field_is_required' && pno_user_cf.is_default && pno_user_cf.field_id === 'email') {
+			if ( api.getFieldValue( 'field_is_required' ) === false ) {
+				api.setFieldValue('field_is_required', true);
+			}
+		}
+	});
+
 });
