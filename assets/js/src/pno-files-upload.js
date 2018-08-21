02da24ea2826f4fd48beedaf29dbba52ed60fa3a
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-08-21 15:32:01 +0200

    remove file pond

--- assets/js/src/pno-files-upload.js
+++ assets/js/src/pno-files-upload.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function ($) {
+
 	$(document.body).on('click', '.wpum-remove-uploaded-file', function () {
 		$(this).closest('.wpum-uploaded-file').remove();
 		return false;
 	});
+
 });
