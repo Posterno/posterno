7bcc7d226806935f56ba59c9872706eafe397fb8
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-09-05 12:31:38 +0200

    assess carbon fields load logic for complex fields

--- includes/datastores/opening-hours.php
+++ includes/datastores/opening-hours.php
@@ -37,6 +37,55 @@ class OpeningHours extends Post_Meta_Datastore {
 	}
 
 	/**
+	 * Retrieve value stored into the database.
+	 *
+	 * @param Field $field the field to load.
+	 * @return mixed
+	 */
+	public function load( Field $field ) {
+
+		$key                = $this->get_key_for_field( $field );
+		$timings            = get_post_meta( $this->get_object_id(), '_listing_opening_hours', true );
+		$day_string         = $this->get_submitted_day( $key );
+		$opening_or_closing = $this->is_opening_or_closing( $key );
+		$value              = '';
+
+		if ( is_a( $field, '\\Carbon_Fields\\Field\\Complex_Field' ) ) {
+
+			$value = [];
+
+			if ( isset( $timings[ $day_string ]['additional_times'] ) && is_array( $timings[ $day_string ]['additional_times'] ) ) {
+
+				foreach ( $timings[ $day_string ]['additional_times'] as $key => $timeset ) {
+
+					$openingstring = $day_string . '_opening';
+					$closingstring = $day_string . '_closing';
+
+					$value[] = [
+						$openingstring => isset( $timeset['opening'] ) ? esc_html( $timeset['opening'] ) : false,
+						$closingstring => isset( $timeset['closing'] ) ? esc_html( $timeset['closing'] ) : false,
+					];
+
+				}
+			}
+		} else {
+
+			if ( isset( $timings[ $day_string ] ) && is_array( $timings[ $day_string ] ) ) {
+				$opening = isset( $timings[ $day_string ]['opening'] ) ? esc_html( $timings[ $day_string ]['opening'] ) : false;
+				$closing = isset( $timings[ $day_string ]['closing'] ) ? esc_html( $timings[ $day_string ]['closing'] ) : false;
+
+				if ( $opening_or_closing == 'opening' ) {
+					$value = $opening;
+				} elseif ( $opening_or_closing == 'closing' ) {
+					$value = $closing;
+				}
+			}
+		}
+
+		return $value;
+	}
+
+	/**
 	 * Save the field value(s)
 	 *
 	 * @param Field $field The field to save.
@@ -71,7 +120,6 @@ class OpeningHours extends Post_Meta_Datastore {
 				if ( $closing ) {
 					$additional_times[ $set ]['closing'] = $closing;
 				}
-
 			}
 
 			pno_update_listing_additional_opening_hours_by_day( $this->get_object_id(), $day_string, $additional_times );
@@ -79,6 +127,7 @@ class OpeningHours extends Post_Meta_Datastore {
 		} else {
 
 			// Determine the day and if it's the opening or closing time.
+			$day_string         = $this->get_submitted_day( $key );
 			$opening_or_closing = $this->is_opening_or_closing( $key );
 			$submitted_time     = $field->get_formatted_value();
 
@@ -118,18 +167,6 @@ class OpeningHours extends Post_Meta_Datastore {
 		$opening_or_closing = substr( $key, strrpos( $key, '_' ) + 1 );
 
 		return $opening_or_closing;
-
-	}
-
-	/**
-	 * Delete the field for the specified object id.
-	 *
-	 * @param Field $field field to deleted.
-	 * @return void
-	 */
-	public function delete( Field $field ) {
-
-		delete_post_meta( $this->get_object_id(), '_listing_opening_hours' );
 
 	}
 
