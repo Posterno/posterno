5b5ec8b2d78330d176bd0f7cd99ba6fbcf728a67
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-09-06 14:55:12 +0200

    make listing custom fields customizable via filters

--- includes/listings/class-pno-listings-custom-fields.php
+++ includes/listings/class-pno-listings-custom-fields.php
@@ -66,62 +66,174 @@ class PNO_Listings_Custom_Fields {
 			'singular_name' => esc_html__( 'Profile' ),
 		);
 
-		Container::make( 'post_meta', esc_html__( 'Listing settings' ) )
-			->where( 'post_type', '=', 'listings' )
-			->add_tab(
-				esc_html__( 'Details' ),
-				array(
-					Field::make( 'radio', 'listing_type', esc_html__( 'Listing type' ) )
-						->set_datastore( new PNO\Datastores\ListingType() )
-						->set_required( true )
-						->set_classes( 'inline-radio-selector' )
-						->add_options(
-							'pno_get_listings_types_for_association'
-						),
-					Field::make( 'text', 'listing_phone_number', esc_html__( 'Phone number' ) )->set_width( 33.33 ),
-					Field::make( 'text', 'listing_email', esc_html__( 'Email address' ) )->set_width( 33.33 ),
-					Field::make( 'text', 'listing_website', esc_html__( 'Website' ) )->set_width( 33.33 ),
-					Field::make( 'complex', 'listing_social_profiles', esc_html__( 'Social profiles' ) )
-						->set_datastore( new PNO\Datastores\SerializeField() )
-						->setup_labels( $social_profiles_labels )
-						->set_collapsed( true )
-						->add_fields(
-							array(
-								Field::make( 'select', 'social_id', esc_html__( 'Network' ) )
-									->add_options( 'pno_get_listings_registered_social_media' )
-									->set_width( 50 ),
-								Field::make( 'text', 'social_url', esc_html__( 'Profile url' ) )->set_width( 50 ),
-							)
-						),
-				)
-			)
-			->add_tab(
-				esc_html__( 'Location' ),
-				array(
-					Field::make( 'map', 'listing_location', esc_html__( 'Location' ) )
-						->set_help_text( esc_html__( 'Search an address or drag and drop the pin on the map to select location.' ) ),
-					Field::make( 'text', 'listing_zipcode', esc_html__( 'Zipcode' ) ),
-				)
-			)
-			->add_tab(
-				esc_html__( 'Media' ),
-				array(
-					Field::make( 'media_gallery', 'listing_gallery_images', esc_html__( 'Images' ) )
-						->set_datastore( new PNO\Datastores\SerializeField() )
-						->set_type( array( 'image' ) ),
-					Field::make( 'oembed', 'listing_media_embed', esc_html__( 'Embed' ) )
-						->set_help_text(
-							sprintf(
-								__( 'Embed videos, images, tweets, audio, and other content into the listing by simply providing the url of the source. <a href="%s" target="_blank">View list of supported embeds.</a>' ),
-								'https://codex.wordpress.org/Embeds'
-							)
-						),
-				)
-			)
-			->add_tab(
-				esc_html__( 'Opening Hours' ),
-				self::get_days_fields()
+		$container = Container::make( 'post_meta', esc_html__( 'Listing settings' ) )
+			->where( 'post_type', '=', 'listings' );
+
+		foreach ( self::get_listing_settings_tabs() as $key => $tab ) {
+
+			$fields = [];
+
+			switch ( $key ) {
+				case 'details':
+				case 'location':
+				case 'media':
+					$fields = self::{"get_listing_{$key}_settings"}();
+					break;
+				case 'hours':
+					$fields = self::get_days_fields();
+					break;
+				default:
+					/**
+					 * Allows developers to define custom settings for the listings post type
+					 * of a given setting tab.
+					 *
+					 * Where $key is the key of the custom tab you've added.
+					 *
+					 * @param array $fields the array where you're going to add the fields for the custom tab.
+					 * @return array
+					 */
+					$fields = apply_filters( "pno_listing_{$key}_settings", $fields );
+					break;
+			}
+
+			$container->add_tab( $tab, $fields );
+
+		}
+
+	}
+
+	/**
+	 * Get the list of tabs for the listing post type custom fields container.
+	 * Powered by Carbon Fields.
+	 *
+	 * @return array
+	 */
+	public static function get_listing_settings_tabs() {
+
+		$tabs = [
+			'details'  => esc_html__( 'Details' ),
+			'location' => esc_html__( 'Location' ),
+			'media'    => esc_html__( 'Media' ),
+			'hours'    => esc_html__( 'Opening Hours' ),
+		];
+
+		/**
+		 * Allow developers to register new tabs within the listing post type settings.
+		 *
+		 * @param array $tabs the list of tabs currently registered.
+		 * @return array
+		 */
+		return apply_filters( 'pno_listing_settings_tabs', $tabs );
+
+	}
+
+	/**
+	 * Define the list of settings for the listings post type.
+	 * These settings are related to the "Details" tab.
+	 *
+	 * @return array
+	 */
+	public static function get_listing_details_settings() {
+
+		$social_profiles_labels = array(
+			'plural_name'   => esc_html__( 'Profiles' ),
+			'singular_name' => esc_html__( 'Profile' ),
+		);
+
+		$settings = [];
+
+		$settings[] = Field::make( 'radio', 'listing_type', esc_html__( 'Listing type' ) )
+			->set_datastore( new PNO\Datastores\ListingType() )
+			->set_required( true )
+			->set_classes( 'inline-radio-selector' )
+			->add_options(
+				'pno_get_listings_types_for_association'
 			);
+
+		$settings[] = Field::make( 'text', 'listing_phone_number', esc_html__( 'Phone number' ) )->set_width( 33.33 );
+		$settings[] = Field::make( 'text', 'listing_email', esc_html__( 'Email address' ) )->set_width( 33.33 );
+		$settings[] = Field::make( 'text', 'listing_website', esc_html__( 'Website' ) )->set_width( 33.33 );
+
+		$settings[] = Field::make( 'complex', 'listing_social_profiles', esc_html__( 'Social profiles' ) )
+			->set_datastore( new PNO\Datastores\SerializeField() )
+			->setup_labels( $social_profiles_labels )
+			->set_collapsed( true )
+			->add_fields(
+				array(
+					Field::make( 'select', 'social_id', esc_html__( 'Network' ) )
+						->add_options( 'pno_get_listings_registered_social_media' )
+						->set_width( 50 ),
+					Field::make( 'text', 'social_url', esc_html__( 'Profile url' ) )->set_width( 50 ),
+				)
+			);
+
+		/**
+		 * Allow developers to customize the settings for the listings post type.
+		 * Settings are powered by Carbon Fields and will be displayed within the "Details" tab.
+		 *
+		 * @param array $settings list of Carbon Fields - fields.
+		 * @return array
+		 */
+		return apply_filters( 'pno_listing_details_settings', $settings );
+
+	}
+
+	/**
+	 * Define the list of settings for the listings post type.
+	 * These settings are related to the "Location" tab.
+	 *
+	 * @return array
+	 */
+	public static function get_listing_location_settings() {
+
+		$settings = [];
+
+		$settings[] = Field::make( 'map', 'listing_location', esc_html__( 'Location' ) )
+			->set_help_text( esc_html__( 'Search an address or drag and drop the pin on the map to select location.' ) );
+
+		$settings[] = Field::make( 'text', 'listing_zipcode', esc_html__( 'Zipcode' ) );
+
+		/**
+		 * Allow developers to customize the settings for the listings post type.
+		 * Settings are powered by Carbon Fields and will be displayed within the "Location" tab.
+		 *
+		 * @param array $settings list of Carbon Fields - fields.
+		 * @return array
+		 */
+		return apply_filters( 'pno_listing_location_settings', $settings );
+
+	}
+
+	/**
+	 * Define the list of settings for the listings post type.
+	 * These settings are related to the "Media" tab.
+	 *
+	 * @return array
+	 */
+	public static function get_listing_media_settings() {
+
+		$settings = [];
+
+		$settings[] = Field::make( 'media_gallery', 'listing_gallery_images', esc_html__( 'Images' ) )
+			->set_datastore( new PNO\Datastores\SerializeField() )
+			->set_type( array( 'image' ) );
+
+		$settings[] = Field::make( 'oembed', 'listing_media_embed', esc_html__( 'Embed' ) )
+			->set_help_text(
+				sprintf(
+					__( 'Embed videos, images, tweets, audio, and other content into the listing by simply providing the url of the source. <a href="%s" target="_blank">View list of supported embeds.</a>' ),
+					'https://codex.wordpress.org/Embeds'
+				)
+			);
+
+		/**
+		 * Allow developers to customize the settings for the listings post type.
+		 * Settings are powered by Carbon Fields and will be displayed within the "Media" tab.
+		 *
+		 * @param array $settings list of Carbon Fields - fields.
+		 * @return array
+		 */
+		return apply_filters( 'pno_listing_media_settings', $settings );
 
 	}
 
@@ -164,15 +276,22 @@ class PNO_Listings_Custom_Fields {
 							->set_picker_options( self::get_timepicker_config() )
 							->set_width( 50 ),
 						Field::make( 'time', $day_string . '_closing', false )
-						->set_attribute( 'placeholder', self::$closing_at )
-						->set_picker_options( self::get_timepicker_config() )
-						->set_width( 50 ),
+							->set_attribute( 'placeholder', self::$closing_at )
+							->set_picker_options( self::get_timepicker_config() )
+							->set_width( 50 ),
 					)
 				);
 
 		}
 
-		return $fields;
+		/**
+		 * Allow developers to customize the settings for the listings post type.
+		 * Settings are powered by Carbon Fields and will be displayed within the "Opening hours" tab.
+		 *
+		 * @param array $settings list of Carbon Fields - fields.
+		 * @return array
+		 */
+		return apply_filters( 'pno_listing_opening_hours_settings', $fields );
 
 	}
 
