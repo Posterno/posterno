46e32a6a08a443f21770389f1a17dbceed7d698b
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-09-19 18:00:46 +0200

    cleanup multiselect js file

--- assets/js/src/pno-multiselect.js
+++ assets/js/src/pno-multiselect.js
@@ -1,6 +1,46 @@
+/*global jQuery:true*/
+/*global pnoMultiselects:true*/
 jQuery(document).ready(function ($) {
-	$('.pno-field-multiselect select, .pno-select-searchable, .pno-listings-category-selector').select2({
-		theme: 'default',
-		placeholder: $(this).data('placeholder'),
-	});
+
+	window.pnoMultiselects = {};
+
+	/**
+	 * Initialize all the functionalities for the select2 scripts.
+	 */
+	pnoMultiselects.init = function () {
+
+		pnoMultiselects.cacheSelectors();
+		pnoMultiselects.transformSelect2();
+
+	}
+
+	/**
+	 * Collect all selectors for the select2 functionality.
+	 */
+	pnoMultiselects.cacheSelectors = function () {
+
+		pnoMultiselects.formSelectFields = $('.pno-field-multiselect select, .pno-select-searchable');
+		pnoMultiselects.listingSubmissionCategory = $('.pno-listings-category-selector');
+
+	}
+
+	/**
+	 * Transform each selector into a select2 element.
+	 */
+	pnoMultiselects.transformSelect2 = function () {
+
+		pnoMultiselects.formSelectFields.select2({
+			theme: 'default',
+			placeholder: $(this).data('placeholder'),
+		});
+
+		pnoMultiselects.listingSubmissionCategory.select2({
+			theme: 'default',
+			placeholder: $(this).data('placeholder'),
+		});
+
+	}
+
+	pnoMultiselects.init();
+
 });
