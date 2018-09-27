198e5f85578dffdfa81832afd4c21c0de9593c51
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-09-27 15:34:22 +0200

    cleanup old code

--- includes/forms-handler/layouts/default-layout.php
+++ includes/forms-handler/layouts/default-layout.php
@@ -89,37 +89,6 @@ class DefaultLayout extends AbstractLayout {
 
 		return ob_get_clean();
 
-		/*
-		$html = '<div>';
-		if ( $field instanceof \PNO\Form\Field\CheckboxField ) {
-			if ( $field->has_label() ) {
-				$html .= "<label>{$field->render()}&nbsp;{$field->get_label()}</label>";
-			} else {
-				$html .= $field->render();
-			}
-		} elseif ( $field instanceof \PNO\Form\Field\AbstractGroup ) {
-			if ( $field->has_label() ) {
-				$html .= "<label>{$field->get_label()}</label>";
-			}
-			foreach ( $field->get_choices() as $choice => $label ) {
-				$html .= "<label>{$field->render_choice($choice)}&nbsp;{$label}</label>";
-			}
-		} else {
-			if ( $field->has_label() ) {
-				$html .= "<label>{$field->get_label()}</label>";
-			}
-			$html .= $field->render();
-		}
-		if ( $field->has_errors() ) {
-			$html .= '<ul class="errors">';
-			foreach ( $field->get_errors() as $error ) {
-				$html .= "<li>{$error}</li>";
-			}
-			$html .= '</ul>';
-		}
-		$html .= '</div>';
-
-		return $html;*/
 	}
 
 }
