2930fc764d0df093ac9c455177a3294cf13140f7
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-10-16 16:47:17 +0100

    Defines the settings for the emails editor.

--- includes/emails/class-pno-emails-editor-settings.php
+++ includes/emails/class-pno-emails-editor-settings.php
@@ -8,6 +8,44 @@
  * @since       0.1.0
  */
 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Class that handles registration of the settings for the emails editor.
+ */
+class PNO_Emails_Editor_Settings {
+
+	/**
+	 * Hook into WordPress and register custom fields for the emails editor.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		add_action( 'carbon_fields_register_fields', [ $this, 'register_settings' ] );
+	}
+
+	/**
+	 * Register settings for the emails editor.
+	 *
+	 * @return void
+	 */
+	public function register_settings() {
+
+		Container::make( 'post_meta', esc_html__( 'Available email template tags:' ) )
+			->where( 'post_type', '=', 'pno_emails' )
+			->add_fields(
+				array(
+					Field::make( 'html', 'pno_email_tags_list' )
+						->set_html( pno_get_emails_tags_list() ),
+				)
+			);
+
+	}
+
+}
+
+( new PNO_Emails_Editor_Settings() )->init();
