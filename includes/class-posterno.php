e2ef9d33314b56bf02813b1cffb20bfed904c6b5
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-10-19 18:13:53 +0100

    prototype representation of a listing custom field

--- includes/class-posterno.php
+++ includes/class-posterno.php
@@ -417,6 +417,7 @@ if ( ! class_exists( 'Posterno' ) ) :
 			require_once PNO_PLUGIN_DIR . 'includes/listings/class-pno-listings-custom-fields-settings.php';
 			require_once PNO_PLUGIN_DIR . 'includes/class-pno-profile-field.php';
 			require_once PNO_PLUGIN_DIR . 'includes/class-pno-registration-field.php';
+			require_once PNO_PLUGIN_DIR . 'includes/class-pno-listing-field.php';
 
 			// Emails.
 			require_once PNO_PLUGIN_DIR . 'includes/emails/class-pno-emails-editor-settings.php';
