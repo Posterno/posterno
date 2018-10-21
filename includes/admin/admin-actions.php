d8179415805e158443c54a7fceaa563fb4164a42
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-10-21 11:39:52 +0100

    add a link back to the all listings fields page

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -147,6 +147,11 @@ function pno_after_custom_fields_post_title() {
 		$admin_url = admin_url( 'edit.php?post_type=listings&page=posterno-custom-registration-form' );
 		echo '<br/><span class="dashicons dashicons-editor-table"></span> <a href="' . esc_url( $admin_url ) . '">' . esc_html__( 'All registration form fields' ) . '</a>';
 
+	} elseif ( $post instanceof WP_Post && isset( $post->post_type ) && $post->post_type == 'pno_listings_fields' ) {
+
+		$admin_url = admin_url( 'edit.php?post_type=listings&page=posterno-custom-listings-fields' );
+		echo '<br/><span class="dashicons dashicons-editor-table"></span> <a href="' . esc_url( $admin_url ) . '">' . esc_html__( 'All listings fields' ) . '</a>';
+
 	}
 
 }
