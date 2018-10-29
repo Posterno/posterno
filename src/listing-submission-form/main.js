f2def41c5e23cf0979aa69193bc79021b01ce7f2
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-10-29 12:10:16 +0000

    Mount vue powered form onto the listing editing page

--- src/listing-submission-form/main.js
+++ src/listing-submission-form/main.js
@@ -11,4 +11,4 @@ import './listing-tags-selector.js'
 import './listing-opening-hours.js'
 import './listing-location-selector.js'
 
-new Vue().$mount('#pno-form-listing_submission_form')
+new Vue().$mount('#pno-form-listing_submission_form, #pno-form-listing_editing_form')
