14db5f0f3d3a4c75ca22c9f677b15e2589b700a2
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-10-30 12:01:54 +0000

    Load selected categories of a listing from the database when viewing the edit form.

--- src/listing-submission-form/listing-category-selector.js
+++ src/listing-submission-form/listing-category-selector.js
@@ -5,6 +5,18 @@ Vue.component('pno-listing-category-selector', {
 			selectedCategories: [],
 		}
 	},
+	mounted() {
+
+		// Load selected categories of a listing from the database when viewing the edit form.
+		var savedCategories = this.getSavedCategories()
+
+		if ( savedCategories.length > 0 ) {
+			savedCategories.forEach((category, index) => {
+				this.selectedCategories.push( category.term_id )
+			});
+		}
+
+	},
 	watch: {
 		/**
 		 * Watch for changes to the vue model and store changes into the frontend field
@@ -15,6 +27,16 @@ Vue.component('pno-listing-category-selector', {
 				document.getElementById('listing_categories').value = JSON.stringify(this.selectedCategories);
 			},
 			deep: true
+		}
+	},
+	methods: {
+		/**
+		 * Get categories loaded into the field from the database.
+		 *
+		 * @returns
+		 */
+		getSavedCategories() {
+			return document.getElementById('listing_categories').value ? JSON.parse( document.getElementById('listing_categories').value ) : false
 		}
 	}
 });
