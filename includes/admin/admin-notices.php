61d804f0d56f9759c6b846133bf2ff12a224ccc6
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2018-12-02 11:22:15 +0000

    remove notices for default fields

--- includes/admin/admin-notices.php
+++ includes/admin/admin-notices.php
@@ -12,66 +12,6 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Tell the user, the field can't be deleted.
- *
- * @return void
- */
-function pno_is_default_field_notice() {
-
-	$screen = get_current_screen();
-
-	if ( $screen instanceof WP_Screen && $screen->id == 'pno_users_fields' ) {
-
-		global $post;
-
-		if ( $post instanceof WP_Post && isset( $post->ID ) && ! posterno()->admin_notices->is_dismissed( 'field_is_default_' . $post->ID ) ) {
-
-			$is_default_field = get_post_meta( $post->ID, 'is_default_field', true );
-
-			if ( $is_default_field ) {
-
-				$message = esc_html__( 'This is a default field. Default fields cannot have their type and meta key changed and can\'t be deleted.' );
-
-				posterno()->admin_notices->register_notice( 'field_is_default_' . $post->ID, 'info', $message );
-
-			}
-		}
-	} elseif ( $screen instanceof WP_Screen && $screen->id == 'pno_signup_fields' ) {
-		global $post;
-
-		if ( $post instanceof WP_Post && isset( $post->ID ) && ! posterno()->admin_notices->is_dismissed( 'field_is_default_' . $post->ID ) ) {
-
-			$is_default_field = carbon_get_post_meta( $post->ID, 'field_is_default' );
-
-			if ( $is_default_field ) {
-
-				$message = esc_html__( 'This is a default field, default fields cannot be deleted.' );
-
-				posterno()->admin_notices->register_notice( 'field_is_default_' . $post->ID, 'info', $message );
-
-			}
-		}
-	} elseif ( $screen instanceof WP_Screen && $screen->id == 'pno_listings_fields' ) {
-		global $post;
-
-		if ( $post instanceof WP_Post && isset( $post->ID ) && ! posterno()->admin_notices->is_dismissed( 'field_is_default_' . $post->ID ) ) {
-
-			$is_default_field = carbon_get_post_meta( $post->ID, 'listing_field_is_default' );
-
-			if ( $is_default_field ) {
-
-				$message = esc_html__( 'This is a default field. Default fields cannot have their type and meta key changed and can\'t be deleted.' );
-
-				posterno()->admin_notices->register_notice( 'field_is_default_' . $post->ID, 'info', $message );
-
-			}
-		}
-	}
-
-}
-add_action( 'admin_head', 'pno_is_default_field_notice' );
-
-/**
  * Display a notice when the avatar field is disabled and the user is editing the field.
  *
  * @return void
