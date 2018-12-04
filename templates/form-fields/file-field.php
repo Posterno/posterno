6acbb51ea6745a5d7f79f224db18bc8723772217
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2018-12-04 17:57:25 +0000

    start rewrite of file field type

--- templates/form-fields/file-field.php
+++ templates/form-fields/file-field.php
@@ -17,62 +17,16 @@
 defined( 'ABSPATH' ) || exit;
 
 $allowed_mime_types = array_keys( ! empty( $data->get_allowed_mime_types() ) ? $data->get_allowed_mime_types() : get_allowed_mime_types() );
-$field_name         = $data->get_name();
-$field_name        .= ! empty( $data->is_multiple() ) ? '[]' : '';
+$field_name         = $data->get_object_meta_key();
+$field_name        .= $data->is_multiple() ? '[]' : '';
 $file_size          = $data->get_maxsize() ? $data->get_maxsize() : false;
 
-// Determine the type of form we're working with.
-$form_type = 'user_meta';
-
-if ( $form_type === 'user_meta' ) {
-	$field_id = $data->get_id() === 'avatar' ? 'current_user_avatar' : $data->get_id();
-	$value    = carbon_get_user_meta( get_current_user_id(), $field_id );
-} else {
-	$value = carbon_get_post_meta( get_current_user_id(), $data->get_id() );
-}
-
-$stored_value = $value;
-
-if ( is_array( $stored_value ) && isset( $stored_value['url'] ) ) {
-	$stored_value = $stored_value['url'];
-}
-
 ?>
-
-<?php if ( ! empty( $stored_value ) ) : ?>
-	<?php if ( is_array( $stored_value ) && ! isset( $stored_value ) ) : ?>
-		<?php foreach ( $stored_value as $value ) : ?>
-			<?php
-				posterno()->templates
-					->set_template_data(
-						[
-							'key'   => $data->get_id(),
-							'name'  => 'current_' . $data->get_id(),
-							'value' => $value,
-						]
-					)
-					->get_template_part( 'form-fields/file', 'uploaded' );
-			?>
-		<?php endforeach; ?>
-	<?php elseif ( $stored_value ) : ?>
-			<?php
-				posterno()->templates
-					->set_template_data(
-						[
-							'key'   => $data->get_id(),
-							'name'  => 'current_' . $data->get_id(),
-							'value' => $stored_value,
-						]
-					)
-					->get_template_part( 'form-fields/file', 'uploaded' );
-			?>
-	<?php endif; ?>
-<?php endif; ?>
 
 <input
 	type="file"
 	<?php pno_form_field_input_class( $data ); ?>
-	name="<?php echo esc_attr( $data->get_object_meta_key() ); ?>"
+	name="<?php echo esc_attr( $field_name ); ?>"
 	id="pno-field-<?php echo esc_attr( $data->get_object_meta_key() ); ?>"
 	aria-describedby="<?php echo esc_attr( $data->get_id() ); ?>"
 	<?php if ( $data->is_multiple() ) echo 'multiple'; //phpcs:ignore ?>
