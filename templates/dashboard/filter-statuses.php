fe5ad704f2751c63aafedbb4dd7987ea0f48696a
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2018-12-21 13:31:07 +0000

    add hooks to the grid filter

--- templates/dashboard/filter-statuses.php
+++ templates/dashboard/filter-statuses.php
@@ -11,6 +11,7 @@
  * the readme will list any important changes.
  *
  * @version 1.0.0
+ * @package Posterno
  */
 
 // Exit if accessed directly.
--- templates/listings/results-grid-filter.php
+++ templates/listings/results-grid-filter.php
@@ -20,10 +20,26 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div class="btn-group" role="group" aria-label="<?php esc_html_e( 'Grid layout' ); ?>">
+
+	<?php
+	/**
+	 * Hook: loads before the listings results grid filter.
+	 */
+	do_action( 'pno_listings_results_before_grid_filter' );
+	?>
+
 	<button type="button" class="btn btn-outline-secondary" aria-label="<?php esc_html_e( 'List layout' ); ?>">
 		<i class="fas fa-list-ul"></i>
 	</button>
 	<button type="button" class="btn btn-outline-secondary" aria-label="<?php esc_html_e( 'Grid layout' ); ?>">
 		<i class="fas fa-th"></i>
 	</button>
+
+	<?php
+	/**
+	 * Hook: loads after the listings results grid filter.
+	 */
+	do_action( 'pno_listings_results_after_grid_filter' );
+	?>
+
 </div>
