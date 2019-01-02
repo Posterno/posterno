9540145e3c4b38064b5cb66029c4c0d7d5de0c42
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-01-02 12:08:08 +0000

    remove functionality to clear pages transient

--- includes/admin/admin-actions.php
+++ includes/admin/admin-actions.php
@@ -12,21 +12,6 @@
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Delete cached list of pages when a page is updated or created.
- * This is needed to refresh the list of available pages for the options panel.
- *
- * @param string $post_id
- * @return void
- */
-function pno_delete_pages_transient( $post_id ) {
-	if ( wp_is_post_revision( $post_id ) ) {
-		return;
-	}
-	delete_transient( 'pno_get_pages' );
-}
-add_action( 'save_post_page', 'pno_delete_pages_transient' );
-
-/**
  * Determines when the custom shortcodes editor can be loaded.
  *
  * @access public
