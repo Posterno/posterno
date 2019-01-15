5b97348cf8f4b3e1508039cb134b52d525cfe102
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-01-15 13:55:22 +0000

    introduce new template files for profile components

---
+++ templates/profile/about.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the about component content on profile pages.
+ *
+ * This template can be overridden by copying it to yourtheme/pno/profile/about.php
+ *
+ * HOWEVER, on occasion PNO will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @version 1.0.0
+ * @package posterno
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
