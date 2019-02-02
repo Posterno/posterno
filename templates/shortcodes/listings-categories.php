2e1c324094710cce32f3c8c83398b233927438fd
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-02 16:03:22 +0000

    completed categories list shortcode

--- templates/shortcodes/listings-categories.php
+++ templates/shortcodes/listings-categories.php
@@ -31,55 +31,71 @@ if ( ! is_array( $terms ) || empty( $terms ) ) {
 	return;
 }
 
+$show_subcategories = isset( $data->subcategories ) && $data->subcategories === 'yes' ? true : false;
+
 ?>
 
 <div class="pno-listings-categories-list">
 
-	<div class="card-deck">
+	<div class="row">
 
-		<?php foreach ( $terms as $listing_category ) : ?>
+	<?php foreach ( $terms as $listing_category ) : ?>
 
-			<ul class="list-unstyled">
+		<div class="col-md-4">
+
+			<ul class="list-unstyled m-0 mb-3">
 				<li>
-					<a href="<?php echo esc_url( get_term_link( $listing_category ) ); ?>" class="mb-3">
+					<a href="<?php echo esc_url( get_term_link( $listing_category ) ); ?>" class="d-block mb-2 parent-category">
 						<strong><?php echo esc_html( $listing_category->name ); ?></strong>
+
+						<?php if ( isset( $listing_category->count ) && absint( $listing_category->count ) > 0 ) : ?>
+							<span class="badge badge-pill badge-secondary ml-2"><?php echo absint( $listing_category->count ); ?></span>
+						<?php endif; ?>
 					</a>
 
 					<?php
 
-					$children = get_term_children( $listing_category->term_id, 'listings-categories' );
+					if ( $show_subcategories ) {
 
-					if ( ! empty( $children ) && is_array( $children ) ) {
+						$children = get_term_children( $listing_category->term_id, 'listings-categories' );
 
-						echo '<ul class="list-unstyled">';
+						if ( ! empty( $children ) && is_array( $children ) ) {
 
-						foreach ( $children as $child_term_id ) {
+							echo '<ul class="list-unstyled m-0">';
 
-							$child_category = get_term_by( 'id', absint( $child_term_id ), 'listings-categories' );
+							foreach ( $children as $child_term_id ) {
 
-							if ( $child_category instanceof WP_Term ) :
-								?>
-									<li>
-										<a href="<?php echo esc_url( get_term_link( $child_category ) ); ?>">
-											<?php echo esc_html( $child_category->name ); ?>
-										</a>
-									</li>
-								<?php
-							endif;
+								$child_category = get_term_by( 'id', absint( $child_term_id ), 'listings-categories' );
+
+								if ( $child_category instanceof WP_Term ) :
+									?>
+										<li class="d-flex justify-content-between align-items-center mb-1">
+											<a href="<?php echo esc_url( get_term_link( $child_category ) ); ?>">
+												<?php echo esc_html( $child_category->name ); ?>
+											</a>
+											<?php if ( isset( $child_category->count ) && absint( $child_category->count ) > 0 ) : ?>
+												<span class="badge badge-pill badge-light"><?php echo absint( $child_category->count ); ?></span>
+											<?php endif; ?>
+										</li>
+									<?php
+								endif;
+
+							}
+
+							echo '</ul>';
 
 						}
-
-						echo '</ul>';
 
 					}
 
 					?>
 
-
 				</li>
 			</ul>
 
-		<?php endforeach; ?>
+		</div>
+
+	<?php endforeach; ?>
 
 	</div>
 
