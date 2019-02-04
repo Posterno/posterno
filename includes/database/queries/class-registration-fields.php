7907fc4330ebf57e53a8081b1c1f04a25a201bcd
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-04 12:12:18 +0000

    doc block will be updated at later point

--- includes/database/queries/class-registration-fields.php
+++ includes/database/queries/class-registration-fields.php
@@ -97,49 +97,12 @@ class Registration_Fields extends Query {
 	/** Methods ***************************************************************/
 
 	/**
-	 * Sets up the customer query, based on the query vars passed.
+	 * Sets up the query, based on the query vars passed.
 	 *
 	 * @since 0.1.0
 	 * @access public
 	 *
-	 * @param string|array $query {
-	 *     Optional. Array or query string of customer query parameters. Default empty.
-	 *
-	 *     @type int          $id                   An customer ID to only return that customer. Default empty.
-	 *     @type array        $id__in               Array of customer IDs to include. Default empty.
-	 *     @type array        $id__not_in           Array of customer IDs to exclude. Default empty.
-	 *     @type int          $customer_id          A customer ID to only return that object. Default empty.
-	 *     @type array        $customer_id__in      Array of customer IDs to include. Default empty.
-	 *     @type array        $customer_id__not_in  Array of customer IDs to exclude. Default empty.
-	 *     @type string       $type                 Limit results to those affiliated with a given type. Default empty.
-	 *     @type array        $type__in             Array of types to include affiliated orders for. Default empty.
-	 *     @type array        $type__not_in         Array of types to exclude affiliated orders for. Default empty.
-	 *     @type string       $status               An address statuses to only return that address. Default empty.
-	 *     @type array        $status__in           Array of address statuses to include. Default empty.
-	 *     @type array        $status__not_in       Array of address statuses to exclude. Default empty.
-	 *     @type string       $email                An email address to only return that email address. Default empty.
-	 *     @type array        $email__in            Array of email addresses to include. Default empty.
-	 *     @type array        $email__not_in        Array of email addresses to exclude. Default empty.
-	 *     @type array        $date_query           Query all datetime columns together. See WP_Date_Query.
-	 *     @type array        $date_created_query   Date query clauses to limit customers by. See WP_Date_Query.
-	 *                                              Default null.
-	 *     @type array        $date_modified_query  Date query clauses to limit by. See WP_Date_Query.
-	 *                                              Default null.
-	 *     @type bool         $count                Whether to return a customer count (true) or array of customer objects.
-	 *                                              Default false.
-	 *     @type string       $fields               Item fields to return. Accepts any column known names
-	 *                                              or empty (returns an array of complete customer objects). Default empty.
-	 *     @type int          $number               Limit number of customers to retrieve. Default 100.
-	 *     @type int          $offset               Number of customers to offset the query. Used to build LIMIT clause.
-	 *                                              Default 0.
-	 *     @type bool         $no_found_rows        Whether to disable the `SQL_CALC_FOUND_ROWS` query. Default true.
-	 *     @type string|array $orderby              Accepts 'id', 'date_created', 'start_date', 'end_date'.
-	 *                                              Also accepts false, an empty array, or 'none' to disable `ORDER BY` clause.
-	 *                                              Default 'id'.
-	 *     @type string       $order                How to order results. Accepts 'ASC', 'DESC'. Default 'DESC'.
-	 *     @type string       $search               Search term(s) to retrieve matching customers for. Default empty.
-	 *     @type bool         $update_cache         Whether to prime the cache for found customers. Default false.
-	 * }
+	 * @param string|array $query the query arguments.
 	 */
 	public function __construct( $query = array() ) {
 		parent::__construct( $query );
