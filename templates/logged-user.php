3a353ed7b1703e55c9ff057a141412d416c3e576
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-05 10:27:34 +0000

    display the manage listings link only if the user has submitted listings

--- templates/logged-user.php
+++ templates/logged-user.php
@@ -19,15 +19,29 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div class="alert alert-info" role="alert">
-	<p><?php printf( __( 'Hello %1$s ( not %1$s? <a href="%2$s" class="alert-link">Log out</a> )' ), '<strong>' . $data->user->display_name . '</strong>', wp_logout_url() ); ?></p>
+	<p><?php printf( __( 'Hello %1$s ( not %1$s? <a href="%2$s" class="alert-link">Log out</a> )' ), '<strong>' . esc_html( $data->user->display_name ) . '</strong>', esc_url( wp_logout_url() ) ); ?></p>
 	<p class="mb-0">
 		<?php
-		printf(
-			__( 'From your account dashboard you can <a href="%1$s">manage your listings</a>, <a href="%2$s">edit your password</a> and <a href="%3$s">customize your account details</a>.' ),
-			pno_get_dashboard_navigation_item_url( 'manage-listings' ),
-			pno_get_dashboard_navigation_item_url( 'password' ),
-			pno_get_dashboard_navigation_item_url( 'edit-account' )
-		);
+
+		if ( pno_user_has_submitted_listings( $data->user->ID ) ) {
+
+			printf(
+				__( 'From your account dashboard you can <a href="%1$s">manage your listings</a>, <a href="%2$s">edit your password</a> and <a href="%3$s">customize your account details</a>.' ),
+				esc_url( pno_get_dashboard_navigation_item_url( 'manage-listings' ) ),
+				esc_url( pno_get_dashboard_navigation_item_url( 'password' ) ),
+				esc_url( pno_get_dashboard_navigation_item_url( 'edit-account' ) )
+			);
+
+		} else {
+
+			printf(
+				__( 'From your account dashboard you can <a href="%1$s">edit your password</a> and <a href="%2$s">customize your account details</a>.' ),
+				esc_url( pno_get_dashboard_navigation_item_url( 'password' ) ),
+				esc_url( pno_get_dashboard_navigation_item_url( 'edit-account' ) )
+			);
+
+		}
+
 		?>
 	</p>
 </div>
