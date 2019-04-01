133cbaa399b10a8671a7448d7cf36e020d401ffa
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-01 16:14:31 +0100

    fix: dropdown/radio/checkboxes not storing data in proper way

--- includes/fields/integration/class-listing.php
+++ includes/fields/integration/class-listing.php
@@ -406,7 +406,7 @@ class Listing {
 						}
 
 						if ( $type == 'select' || $type == 'set' || $type == 'multiselect' || $type == 'radio' ) {
-							$admin_fields[] = Field::make( $type, $custom_listing_field->get_object_meta_key(), $custom_listing_field->get_name() )->add_options( $custom_listing_field->get_options() );
+							$admin_fields[] = Field::make( $type, $custom_listing_field->get_object_meta_key(), $custom_listing_field->get_name() )->add_options( $custom_listing_field->get_options() )->set_datastore( new \PNO\Datastores\SerializeComplexField() );
 						} elseif ( $type == 'file' ) {
 							if ( $custom_listing_field->is_multiple() ) {
 								$admin_fields[] = Field::make( 'media_gallery', $custom_listing_field->get_object_meta_key(), $custom_listing_field->get_name() )->set_datastore( new \PNO\Datastores\SerializeComplexField() );
