a416abc177da6f978771266e941607f887b142e6
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-02 16:08:33 +0100

    Added featured icon and image to types and cats

--- includes/listings/class-pno-listings-terms-custom-fields.php
+++ includes/listings/class-pno-listings-terms-custom-fields.php
@@ -24,10 +24,29 @@ class PNO_Listing_Terms_Custom_Fields {
 	 *
 	 * @return void
 	 */
-	public static function init() {
+	public function init() {
 
-		add_action( 'carbon_fields_register_fields', [ __class__, 'register_type_settings' ] );
-		add_action( 'carbon_fields_register_fields', [ __class__, 'register_categories_settings' ] );
+		add_action( 'carbon_fields_register_fields', [ $this, 'register_type_settings' ] );
+		add_action( 'carbon_fields_register_fields', [ $this, 'register_categories_settings' ] );
+
+	}
+
+	/**
+	 * Get common settings available to all listings taxonomies.
+	 *
+	 * @return array
+	 */
+	private function get_common_settings() {
+
+		$fields = [];
+
+		$fields[] = Field::make( 'text', 'term_icon', esc_html__( 'Custom icon class' ) )
+			->set_help_text( esc_html__( 'Custom css class of an icon. Eg: calendar-week', 'posterno' ) );
+
+		$fields[] = Field::make( 'image', 'term_image', esc_html__( 'Featured image' ) )
+			->set_help_text( esc_html__( 'The featured image is not prominent by default; however, some themes may show it.' ) );
+
+		return $fields;
 
 	}
 
@@ -36,21 +55,21 @@ class PNO_Listing_Terms_Custom_Fields {
 	 *
 	 * @return void
 	 */
-	public static function register_type_settings() {
+	public function register_type_settings() {
 
-		if ( ! pno_get_option( 'submission_categories_associated', false ) ) {
-			return;
+		$fields = [];
+
+		if ( pno_get_option( 'submission_categories_associated', false ) ) {
+			$fields[] = Field::make( 'multiselect', 'associated_categories', esc_html__( 'Associated categories', 'posterno' ) )
+				->set_help_text( esc_html__( 'Select one or more listings categories that you wish to assign to this listing type.', 'posterno' ) )
+				->add_options( 'pno_get_listings_categories_for_association' );
 		}
+
+		$fields = array_merge( $fields, $this->get_common_settings() );
 
 		Container::make( 'term_meta', esc_html__( 'Listing type settings', 'posterno' ) )
 			->where( 'term_taxonomy', '=', 'listings-types' )
-			->add_fields(
-				array(
-					Field::make( 'multiselect', 'associated_categories', esc_html__( 'Associated categories', 'posterno' ) )
-						->set_help_text( esc_html__( 'Select one or more listings categories that you wish to assign to this listing type.', 'posterno' ) )
-						->add_options( 'pno_get_listings_categories_for_association' ),
-				)
-			);
+			->add_fields( $fields );
 
 	}
 
@@ -59,21 +78,25 @@ class PNO_Listing_Terms_Custom_Fields {
 	 *
 	 * @return void
 	 */
-	public static function register_categories_settings() {
+	public function register_categories_settings() {
 
 		if ( ! pno_get_option( 'submission_tags_associated', false ) ) {
 			return;
 		}
 
+		$fields = [];
+
+		if ( pno_get_option( 'submission_tags_associated', false ) ) {
+			$fields[] = Field::make( 'multiselect', 'associated_tags', esc_html__( 'Associated listing tags', 'posterno' ) )
+				->set_help_text( esc_html__( 'Select one or more listings tags that you wish to assign to this listing category.', 'posterno' ) )
+				->add_options( 'pno_get_listings_tags_for_association' );
+		}
+
+		$fields = array_merge( $fields, $this->get_common_settings() );
+
 		Container::make( 'term_meta', esc_html__( 'Listing categories settings', 'posterno' ) )
 			->where( 'term_taxonomy', '=', 'listings-categories' )
-			->add_fields(
-				[
-					Field::make( 'multiselect', 'associated_tags', esc_html__( 'Associated listing tags', 'posterno' ) )
-						->set_help_text( esc_html__( 'Select one or more listings tags that you wish to assign to this listing category.', 'posterno' ) )
-						->add_options( 'pno_get_listings_tags_for_association' ),
-				]
-			);
+			->add_fields( $fields );
 
 	}
 
