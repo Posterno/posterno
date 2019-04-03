84a2b082f3f24b7db05383d69df52991f3298e45
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-03 14:21:34 +0100

    version bump

--- includes/class-posterno.php
+++ includes/class-posterno.php
@@ -187,7 +187,7 @@ if ( ! class_exists( 'Posterno' ) ) :
 		private function setup_constants() {
 			// Plugin version.
 			if ( ! defined( 'PNO_VERSION' ) ) {
-				define( 'PNO_VERSION', '0.3.1' );
+				define( 'PNO_VERSION', '0.4.0' );
 			}
 			// Plugin Root File.
 			if ( ! defined( 'PNO_PLUGIN_FILE' ) ) {
