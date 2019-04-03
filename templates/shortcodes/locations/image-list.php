2c52092031c0827744bfaefc623ea3114f4c0c6a
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-03 11:33:00 +0100

    introduce new template file for listings locations shortcode

---
+++ templates/shortcodes/locations/image-list.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This template displays a single location within the listings locations shortcode, specifically
+ * only when the location does have a featured image.
+ *
+ * This template can be overridden by copying it to yourtheme/posterno/shortcodes/locations/image-list.php
+ *
+ * HOWEVER, on occasion PNO will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @version 1.0.0
+ * @package posterno
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
