61988d45e4fbfe5091481207c44fb2e3e8de2f5b
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-06-21 20:07:19 +0100

    version bump

--- posterno.php
+++ posterno.php
@@ -7,7 +7,7 @@
  * Author URI:      https://posterno.com
  * Text Domain:     posterno
  * Domain Path:     /languages
- * Version:         1.0.1
+ * Version:         1.0.2
  *
  * Posterno is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
