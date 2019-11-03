a8771ae6acaa05f8f15864db211f65968f4b1c03
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-11-03 14:22:56 +0000

    Fix: set custom posts_per_page attribute when not set by parent query

--- includes/listings/listings-actions.php
+++ includes/listings/listings-actions.php
@@ -37,7 +37,9 @@ function pno_adjust_listings_query( $query ) {
 	if ( ! is_admin() && $query->is_main_query() && $is_listing_page || ( ! is_admin() && isset( $query->query['is_listings_query'] ) && $query->query['is_listings_query'] === true ) ) {
 
 		// Set posts per page.
-		$query->set( 'posts_per_page', pno_get_listings_results_per_page_active_option() );
+		if ( ! isset( $query->query['posts_per_page'] ) ) {
+			$query->set( 'posts_per_page', pno_get_listings_results_per_page_active_option() );
+		}
 
 		// Set sorting order.
 		$active_order = pno_get_listings_results_order_active_filter();
