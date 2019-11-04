632a7a2fe494faf22661eef1c0b44c0794286d7c
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-11-04 12:36:09 +0000

    added support for taxonomies query

--- templates/blocks/block-listings.php
+++ templates/blocks/block-listings.php
@@ -16,20 +16,19 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-print_r( $data );
-
 $posts_per_page = isset( $data->number ) ? absint( $data->number ) : absint( pno_get_listings_results_per_page_options() );
 $featured       = isset( $data->show_featured_only ) && $data->show_featured_only === true;
 $pagination     = isset( $data->pagination ) && $data->pagination === true;
 $layout         = isset( $_GET['layout'] ) ? pno_get_listings_results_active_layout() : ( isset( $data->layout ) && array_key_exists( $data->layout, pno_get_listings_layout_options() ) ? $data->layout : pno_get_listings_results_active_layout() );
-$display_sorter = isset( $data->sorter ) && $data->sorter === true;
-$sort_method    = isset( $data->sort ) ? $data->sort : false;
 $author_id      = isset( $data->user_id ) ? absint( $data->user_id ) : false;
+$taxonomies     = isset( $data->taxonomies ) && ! empty( $data->taxonomies ) ? pno_clean( explode( ',', $data->taxonomies ) ) : [];
+$terms          = isset( $data->terms ) && ! empty( $data->terms ) ? pno_clean( json_decode( $data->terms, true ) ) : [];
 
 $args = [
-	'post_type'      => 'listings',
-	'pno_search'     => true,
-	'posts_per_page' => $posts_per_page,
+	'post_type'         => 'listings',
+	'pno_search'        => true,
+	'is_listings_query' => true,
+	'posts_per_page'    => $posts_per_page,
 ];
 
 // Add pagination support.
@@ -52,54 +51,35 @@ if ( $author_id ) {
 	$args['author'] = $author_id;
 }
 
-if ( $sort_method ) {
-
-	$sorters                   = '';
-	$enabled_featuring_sorters = pno_get_option( 'listings_featured_in_sorters', [] );
-	$listings_can_be_featured  = pno_listings_can_be_featured();
-
-	switch ( $sort_method ) {
-		case 'newest':
-			$sorters = [
-				'menu_order' => 'ASC',
-				'date'       => 'DESC',
+// Add support for taxonomy terms filtering.
+if ( is_array( $taxonomies ) && ! empty( $taxonomies ) ) {
+	$filter = [];
+	foreach ( $taxonomies as $taxonomy_name ) {
+		$specific_terms = isset( $terms[ $taxonomy_name ] ) && ! empty( $terms[ $taxonomy_name ] ) ? $terms[ $taxonomy_name ] : false;
+		if ( $specific_terms ) {
+			$filter[] = [
+				'taxonomy' => $taxonomy_name,
+				'field'    => 'term_id',
+				'terms'    => $specific_terms,
 			];
-			break;
-		case 'oldest':
-			$sorters = [
-				'menu_order' => 'ASC',
-				'date'       => 'ASC',
-			];
-			break;
-		case 'title':
-			$sorters = [
-				'menu_order' => 'ASC',
-				'title'      => 'ASC',
-			];
-			break;
-		case 'title_za':
-			$sorters = [
-				'menu_order' => 'ASC',
-				'title'      => 'DESC',
-			];
-			break;
-		case 'random':
-			$sorters = 'rand';
-			break;
+		}
 	}
-
-	if ( isset( $sorters ) && ! empty( $sorters ) ) {
-		if ( ! in_array( $sort_method, $enabled_featuring_sorters, true ) && isset( $sorters['menu_order'] ) ) {
-			unset( $sorters['menu_order'] );
-		}
-		if ( ! $listings_can_be_featured && isset( $sorters['menu_order'] ) ) {
-			unset( $sorters['menu_order'] );
-		}
-		$args['orderby'] = $sorters;
+	if ( ! empty( $filter ) ) {
+		$args['tax_query'] = $filter;
 	}
 }
 
 $i = '';
+
+/**
+ * Filter: allow developers to modify the WP_Query arguments for listings
+ * generated through the listings block.
+ *
+ * @param array $args WP_Query arguments list.
+ * @param object $data attributes sent through the block.
+ * @return array
+ */
+$args = apply_filters( 'pno_listings_block_query', $args, $data );
 
 $query = new WP_Query( $args );
 
