8b6f0f96eadf2748d809cc9ffaa46f0c90277416
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-11-25 16:07:27 +0000

    change description

--- includes/admin/class-pno-options-panel.php
+++ includes/admin/class-pno-options-panel.php
@@ -765,7 +765,7 @@ class OptionsPanel {
 			->add_options( 'pno_get_marker_text_fields' );
 
 		$settings[] = Field::make( 'checkbox', 'map_gdpr', esc_html__( 'Display a request for consent before loading maps', 'posterno' ) )
-			->set_help_text( esc_html__( 'Enable this option to prevent the map from automatically loading by requesting consent from the user first.', 'posterno' ) );
+			->set_help_text( esc_html__( 'Enable this option to prevent the map from automatically loading by requesting consent from the user first. Useful if you need to meet GDPR requirements.', 'posterno' ) );
 
 		return $settings;
 
