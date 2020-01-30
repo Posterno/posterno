d5d1a7a5104e46bade2a89a06cd5237b4b434405
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-01-30 17:01:25 +0000

    version bump

--- includes/class-posterno.php
+++ includes/class-posterno.php
@@ -218,7 +218,7 @@ if ( ! class_exists( 'Posterno' ) ) :
 		private function setup_constants() {
 			// Plugin version.
 			if ( ! defined( 'PNO_VERSION' ) ) {
-				define( 'PNO_VERSION', '1.2.5' );
+				define( 'PNO_VERSION', '1.2.6' );
 			}
 			// Plugin Root File.
 			if ( ! defined( 'PNO_PLUGIN_FILE' ) ) {
